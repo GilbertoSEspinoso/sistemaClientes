3fe8c455381c6b197291695db0945fae27112297
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-06-30 10:58:19 -0300

    initial project

---
+++ src/main/java/com/devsuperior/sistemaClientes/SistemaClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistemaClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/sistemaClientes/SistemaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistemaClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
