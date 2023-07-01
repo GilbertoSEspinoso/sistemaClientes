7bd58e8ea9f972ea88ee02845e384621f9fef125
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-06-30 22:13:18 -0300

    Client/seed

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Balin Filho de Fufin', 03003799789, 2.300, '1961-07-25', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Dwalin Fundo da Rocha', 36279907066, 2.500, '1965-10-20', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Fili Herdeiro de Durin', 92395153052, 2.850, '1988-02-19', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Kili Pedra de Fogo', 42259977073, 2.900, '1991-07-02', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Dori da Faca Afiada', 76695387087, 2.500, '1980-02-15', 4);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Nori BarbaLonga', 10175117055, 3.100, '1978-10-10', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Gimli, filho de Glóin', 66235997035, 3.000, '1985-11-05', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Óin, filho de Gróin', 13376545073, 2.900, '1982-07-03', 8);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Thorin Escudo de Carvalho', 62710358093, 8.500, '1980-03-12', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Bilbo Adalberto Bolseiro', 20066849063, 4.000, '1995-08-17', 1);
