2fbecdd51584b6cfc3aa3ccf02d6b51b7a95d131
Gilberto Espinoso <gilbertosilvaespinosa@gmail.com>
2023-07-01 00:51:12 -0300

    Client find all paged

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Balin Filho de Fudin', 03003799789, 2.300, '1961-07-25', 2);
-INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Dwalin Fundo da Rocha', 36279907066, 2.500, '1965-10-20', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Dwalin Funda Rocha', 36279907066, 2.500, '1965-10-20', 1);
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Fili Herdeiro de Durin', 92395153052, 2.850, '1988-02-19', 4);
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Kili Pedra de Fogo', 42259977073, 2.900, '1991-07-02', 5);
 INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Dori da Faca Afiada', 76695387087, 2.500, '1980-02-15', 4);
